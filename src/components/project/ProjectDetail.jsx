892625b15e1abd9abe66b245b446432e117f1e96
Sabahna <sabahna@Sabahnas-MacBook-Air.local>
2023-08-30 23:05:54 +0630

    project testing success

--- src/components/project/ProjectDetail.jsx
+++ src/components/project/ProjectDetail.jsx
@@ -1,99 +1,76 @@
-import React from "react";
+import { useState } from "react";
 
-const ProjectDetail = () => {
+export default function ProjectDetail({ item }) {
+  const [showModal, setShowModal] = useState(false);
   return (
-    <div>
-      <button
-        type="button"
-        class="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
-        data-te-toggle="modal"
-        data-te-target="#exampleModalScrollable"
-        data-te-ripple-init
-        data-te-ripple-color="light"
-      >
-        Launch demo modal dialog scrollable
-      </button>
-
-      <div
-        data-te-modal-init
-        class="fixed left-0 top-0 z-[1055] hidden h-full w-full overflow-y-auto overflow-x-hidden outline-none"
-        id="exampleModalScrollable"
-        tabindex="-1"
-        aria-labelledby="exampleModalScrollableLabel"
-        aria-hidden="true"
-      >
-        <div
-          data-te-modal-dialog-ref
-          class="pointer-events-none relative h-[calc(100%-1rem)] w-auto translate-y-[-50px] opacity-0 transition-all duration-300 ease-in-out min-[576px]:mx-auto min-[576px]:mt-7 min-[576px]:h-[calc(100%-3.5rem)] min-[576px]:max-w-[500px]"
+    <>
+      <div className="flex items-center justify-center">
+        <button
+          className="py-3 bg-cushadow hover:bg-cu px-6 pb-2  text-xs font-medium uppercase leading-normal text-white "
+          type="button"
+          onClick={() => setShowModal(true)}
         >
-          <div class="pointer-events-auto relative flex max-h-[100%] w-full flex-col overflow-hidden rounded-md border-none bg-white bg-clip-padding text-current shadow-lg outline-none dark:bg-neutral-600">
-            <div class="flex flex-shrink-0 items-center justify-between rounded-t-md border-b-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50">
-              <h5
-                class="text-xl font-medium leading-normal text-neutral-800 dark:text-neutral-200"
-                id="exampleModalScrollableLabel"
-              >
-                Modal title
-              </h5>
-              <button
-                type="button"
-                class="box-content rounded-none border-none hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none"
-                data-te-modal-dismiss
-                aria-label="Close"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  class="h-6 w-6"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              </button>
-            </div>
-
-            <div class="relative overflow-y-auto p-4">
-              <p>
-                This is some placeholder content to show the scrolling behavior
-                for modals. We use repeated line breaks to demonstrate how
-                content can exceed minimum inner height, thereby showing inner
-                scrolling. When content becomes longer than the predefined
-                max-height of modal, content will be cropped and scrollable
-                within the modal.
-              </p>
-              <div style={{ height: "800px" }}></div>
-              <p>This content should appear at the bottom after you scroll.</p>
-            </div>
-
-            <div class="flex flex-shrink-0 flex-wrap items-center justify-end rounded-b-md border-t-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50">
-              <button
-                type="button"
-                class="inline-block rounded bg-primary-100 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-primary-700 transition duration-150 ease-in-out hover:bg-primary-accent-100 focus:bg-primary-accent-100 focus:outline-none focus:ring-0 active:bg-primary-accent-200"
-                data-te-modal-dismiss
-                data-te-ripple-init
-                data-te-ripple-color="light"
-              >
-                Close
-              </button>
-              <button
-                type="button"
-                class="ml-1 inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
-                data-te-ripple-init
-                data-te-ripple-color="light"
-              >
-                Save changes
-              </button>
+          Details
+        </button>
+      </div>
+      {showModal ? (
+        <>
+          <div className="fixed inset-0 z-10 overflow-y-auto ">
+            <div
+              className="fixed inset-0 w-full h-full bg-dark opacity-40"
+              onClick={() => setShowModal(false)}
+            ></div>
+            <div className="flex items-center min-h-screen px-4 py-8">
+              <div className="relative w-full max-w-6xl	 p-4 mx-auto bg-white rounded-md shadow-lg">
+                <div className="mt-3 flex">
+                  <div className=" text-center md:text-left">
+                    <div class="block rounded-lg ">
+                      <div class="flex flex-wrap">
+                        <div class="block w-full shrink-0 grow-0 basis-auto md:flex md:w-4/12">
+                          <img
+                            src={item?.path}
+                            alt="Trendy Pants and Shoes"
+                            class="w-full rounded-t-lg md:rounded-tr-none md:rounded-bl-lg"
+                          />
+                        </div>
+                        <div class="w-full shrink-0 grow-0 basis-auto md:w-8/12">
+                          <div class="px-6 py-12 md:px-12">
+                            <h2 class="mb-6 pb-2 text-4xl font-bold">
+                              Project Details
+                            </h2>
+                            <h2 class="mb-6 pb-2 text-2xl font-bold">
+                              {item?.title}
+                            </h2>
+                            <p class="mb-6 pb-2 text-neutral-500 dark:text-neutral-300">
+                              Lorem ipsum dolor sit amet, consectetur
+                              adipisicing elit. A soluta corporis voluptate ab
+                              error quam dolores doloremque, quae consectetur.
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="items-center gap-2 mt-3 sm:flex">
+                      <button
+                        className="w-full mt-2 p-2.5 flex-1 text-cu hover:bg-cu hover:text-white rounded-md outline-none border ring-offset-2 ring-indigo-600 focus:ring-2"
+                        onClick={() => setShowModal(false)}
+                      >
+                        View
+                      </button>
+                      <button
+                        className="w-full mt-2 p-2.5 flex-1 text-cu hover:bg-cu hover:text-white rounded-md outline-none border ring-offset-2 ring-indigo-600 focus:ring-2"
+                        onClick={() => setShowModal(false)}
+                      >
+                        Cancel
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-    </div>
+        </>
+      ) : null}
+    </>
   );
-};
-
-export default ProjectDetail;
+}
